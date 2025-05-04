1d26a6328bad0ec4b7c4a7e6f777d6e0cc5cdb53
Kilian <kiliankager29@yahoo.com>
2025-05-04 14:21:12 +0200

    bla

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import p5 from "p5";
 const p = new p5((sketch) => {
     sketch.setup = setup;
 });
-
+//todo: load jason
 function setup() {
     p.createCanvas(400, 400);
     p.background("red");
