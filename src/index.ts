7502337ddb0ba7bd80e265f70a85dfe2e7484799
Kilian <kiliankager29@yahoo.com>
2025-05-04 16:54:09 +0200

    versuche kreise zu färben

--- src/index.ts
+++ src/index.ts
@@ -5,33 +5,36 @@
 4: x variable erhöhen aber nicht in for schleife sondern wie bei Leberkas
 5: es sind 49 objekte im array
 */
-import p5 from "p5";
-let myData: number[]= [] ;
+import p5 from 'p5';
+let myData: number[] = [];
 
 const p = new p5((sketch) => {
-    sketch.setup = setup;
-    sketch.preload = preload;
-    sketch.draw = draw;
+  sketch.setup = setup;
+  sketch.preload = preload;
+  sketch.draw = draw;
 });
 function preload() {
-    //myData = p.loadJSON('https://prototypes.at/kk/') as number[];
-    myData = p.loadJSON('test.json') as number[];
-
+  //myData = p.loadJSON('https://prototypes.at/kk/') as number[];
+  myData = p.loadJSON('test.json') as number[];
 }
 //todo: load jason
 function setup() {
-    console.log(myData);
-    p.createCanvas(innerWidth, innerHeight);
-    p.background("black");
-    //console.log(innerHeight)
+  console.log(myData);
+  p.createCanvas(innerWidth, innerHeight);
+  p.background('black');
+  //console.log(innerHeight)
 }
 function draw() {
-    p.background("black");
-    p.fill("white");
-    p.textSize(32);
 
-    for(let i = 0; i < innerWidth; i += innerWidth / 49){
-    p.circle(i + 13, 46, innerWidth / 50);
+  for (let i = 0; i < innerWidth; i += innerWidth / 49) {
+    for (let x = 0; x < myData.length; x++) {
+        if (myData[x] === 0) {
+            p.fill('red');
+         
+          } else if (myData[x] === 1) {
+            p.fill('green');
+        }
+        p.circle(i + 13, 46, innerWidth / 50);
     }
-
+  }
 }
